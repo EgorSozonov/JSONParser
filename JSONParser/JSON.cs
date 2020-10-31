ab45a9863c2e391632cff98dfb08691e67319684
a <Egor.Sozonov.09@gmail.com>
2020-10-31 23:49:10 +0300

    0

---
+++ JSONParser/JSON.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONParser {
+    public class JSON {
+    }
+
+    public sealed class JSONString : JSON {
+        public string val;
+    }
+
+    public sealed class JSONNumber : JSON {
+        public double val;
+    }
+
+    public sealed class JSONObject : JSON {
+        public Dictionary<string, JSON> dict = new Dictionary<string, JSON>();               
+    }
+
+    public sealed class JSONArray : JSON {
+        public List<JSONObject> arr;
+    }
+}
---
+++ JSONParser/JSONParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONParser {
+    public class JSONParser {
+        public JSON parse(string input) {
+            if (input == "" || input == null) return null;
+
+            var stack = new Stack<JSON>();
+            int lastDelimiter = -1;
+            for (int i = 0; i < input.Length; ++i) {
+                char c = input[i];
+                switch (c) {
+                    case '{':
+                        stack.Push(new JSONObject());
+
+                        break;
+                    case '[':
+                        stack.Push(new JSONArray());
+                        break;
+                    case '}':
+                        stack.Push(new JSONObject());
+
+                        break;
+                    case ']':
+                        stack.Push(new JSONArray());
+                        break;
+                    case '\\':
+                        if (i + 1 < input.Length) {
+                            char nextChar = input[i + 1];
+                            if (nextChar == '{' || nextChar == '}') {
+                                ++i;
+                            }
+                        }
+                            
+                        break;
+                    default:
+                        break;
+                }
+                ++i;
+            }
+            return null;
+        }
+
+        protected static void addChunk(string input, int start, int end, Stack<JSON> stack) {
+            if (end - start > 0) {
+                var top = stack.Peek();
+                if (top is JSONArray arr) {
+                    
+                }
+            }
+        }
+    }
+}
